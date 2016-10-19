a3873a7c2672cfddf5a757f33bbc2527246e3464
Miguel Provencio <miguel.a.provencio@protonmail.com>
2016-10-19 07:03:08 -0600

    disable submit

--- src/Register.vue
+++ src/Register.vue
@@ -3,27 +3,27 @@
     <form id="signUpForm" class="form">
       <div class="form-group">
         <label for='email' class='register-label'>Email Address:</label>
-        <input id='email' type='text' class="register-field" data-h5-errorid="invalid-email" required title='Email is required' v-model='email'>
+        <input id='email' type='email' v-bind:class='{fieldError: validateEmail && emailError}' class="register-field" v-model='email'>
         <div v-if='validateEmail && emailError != ""' class='error'>
           {{ emailError}}
         </div>
       </div>
       <div class="form-group">
         <label for='password' class='register-label'>Password:</label>
-        <input id='password' type='password' class="register-field" data-h5-errorid='invalid-password' required title='Password is required' v-model='password'>
+        <input id='password' type='password' v-bind:class='{fieldError: validatePassword && passwordError}' class="register-field" v-model='password'>
         <div v-if='validatePassword && passwordError != ""' class='error'>
           {{ passwordError }}
         </div>
       </div>
       <div class="form-group">
         <label for='confirmPassword' class='register-label'>Confirm Password:</label>
-        <input id='confirmPassword' type='password' class="register-field" data-h5-errorid='invalid-confirm-password' required title='Confirm Password is required' v-model='confirmPassword'>
+        <input id='confirmPassword' type='password' v-bind:class='{fieldError: validateConfirmPassword && confirmPasswordError}' class="register-field" v-model='confirmPassword'>
         <div v-if='validateConfirmPassword && confirmPasswordError != ""' class="error">
           {{ confirmPasswordError }}
         </div>
       </div>
       <div class="form-group">
-        <button v-on:click.prevent="clickSignUp" type="submit">Sign Up</button>
+        <button v-on:click.prevent="clickSignUp" type="submit" v-bind:disabled="!valid">Sign Up</button>
       </div>
     </form>
   </div>
@@ -66,7 +66,6 @@
         return ""
       },
       valid() {
-        return true;
         return (this.emailError + this.passwordError + this.confirmPasswordError) == ""
       }
     },
@@ -111,6 +110,9 @@
   }
   input {
     box-sizing: border-box;
+  }
+  .fieldError {
+    border-color: red;
   }
   .error {
     color: red;
