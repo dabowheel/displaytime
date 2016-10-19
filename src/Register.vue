99b4e7ca590a235f8eecdcd311bb93bc241185d3
Miguel Provencio <miguel.a.provencio@protonmail.com>
2016-10-18 19:17:20 -0600

    validate signup

--- src/Register.vue
+++ src/Register.vue
@@ -3,18 +3,18 @@
     <div class="form">
       <div class="form-group">
         <label class='register-label'>Email Address:</label>
-        <input type='text' class="register-field" v-model='email'>
+        <input id='email' type='text' class="register-field" v-model='email'>
       </div>
       <div class="form-group">
         <label class='register-label'>Password:</label>
-        <input type='password' class="register-field" v-model='password'>
+        <input id='password' type='password' class="register-field" v-model='password'>
       </div>
       <div class="form-group">
         <label class='register-label'>Confirm Password:</label>
-        <input type='password' class="register-field" v-model='confirmPassword'>
+        <input id='confirmPassword' type='password' class="register-field" v-model='confirmPassword'>
       </div>
       <div class="form-group">
-        <button>Sign Up</button>
+        <button v-bind:disabled="valid == false">Sign Up</button>
       </div>
     </div>
   </div>
@@ -30,7 +30,44 @@
       }
     },
     computed: {
-      
+      checkEmail() {
+        if (!this.email)
+          return "Email needs a value"
+        if (this.email.indexOf('@') == -1)
+          return "Email must be a valid email address"
+        return ""
+      },
+      checkPassword() {
+        if (!this.password)
+          return "Password needs a value"
+        if (this.password.length < 8)
+          return "Password must be at least 8 characters"
+        return ""
+      },
+      checkConfirmPassword() {
+        if (this.checkPassword)
+          return ""
+        if (!this.confirmPassword)
+          return "Confirm Password needs a value"
+        if (this.confirmPassword != this.password)
+          return "Confirm Password must match Password"
+        return ""
+      },
+      valid() {
+        return (this.checkEmail + this.checkPassword + this.checkConfirmPassword) == ""
+      }
+    },
+    watch: {
+      email() {
+        document.getElementById("email").setCustomValidity(this.checkEmail)
+      },
+      password() {
+        document.getElementById("password").setCustomValidity(this.checkPassword)
+        document.getElementById("confirmPassword").setCustomValidity(this.checkConfirmPassword)
+      },
+      confirmPassword() {
+        document.getElementById("confirmPassword").setCustomValidity(this.checkConfirmPassword)
+      }
     }
   };
 </script>
