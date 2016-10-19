0a6819469cd5e2f3bd1ceb05e196a312f6da4068
Miguel Provencio <miguel.a.provencio@protonmail.com>
2016-10-19 15:12:58 -0600

    toolbar alignment

--- src/Toolbar.vue
+++ src/Toolbar.vue
@@ -6,10 +6,12 @@
       <span v-if="$route.path == '/login'" class="header">Login</span>
     </div>
     <div class="toolbar-links">
-      <router-link v-if="$route.path == '/'" to="/register" class="btn toolbar-link click-link" title="sign up">Sign up</router-link>
-      <router-link v-if="$route.path == '/'" to="/login" class="btn toolbar-link click-link" title="login">Login</router-link>
       <router-link v-if="$route.path == '/'" to="/settings" class="btn toolbar-link icon-link" title="show settings"><i class="fa fa-cog fa-2x" aria-hidden="true"></i></router-link>
       <router-link v-if="$route.path != '/'" to="/" class="btn toolbar-link icon-link" title="close settings"><i class="fa fa-times fa-2x" aria-hidden="true"></i></router-link>
+    </div>
+    <div class="toolbar-links">
+      <router-link v-if="$route.path == '/'" to="/register" class="btn toolbar-link click-link" title="sign up">Sign up</router-link>
+      <router-link v-if="$route.path == '/'" to="/login" class="btn toolbar-link click-link" title="login">Login</router-link>
     </div>
     <div class="toolbar-separator"></div>
   </div>
@@ -55,7 +57,7 @@
     margin-left: .5em;
   }
   .header {
-    line-height: 2em;
+    line-height: 1.3em;
     font-size: 1.5em;
   }
 </style>
